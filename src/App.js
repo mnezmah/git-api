19f53b72ff78e2af2039cb262281acf328fbbe97
mario <mario.nezmah@gmail.com>
2018-10-23 12:09:08 +0200

    added search fucntionality

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react';
+
 import Profile from './Profile';
 import './App.css';
 
 class App extends Component {
+
+constructor(props) {
+  super(props);
+  this.state = {
+    showComponent: false
+  }
+}
+
 
 state = {
   user: {}
@@ -13,34 +22,45 @@ getUser = () => {
   fetch(`http://api.github.com/users/${name}`)
     .then(response => response.json())
     .then(data => {
-    	this.SetState({
+    	this.setState({
     		user:  {
     			name: data.name, 
-    			location: data.location
-    		}
+    			location: data.location,
+    			image: data.avatar_url,
+    			link: data.html_url,
+          followers: data.followers,
+          repos: data.public_repos,
+          since: data.created_at
+    		},
+         showComponent: true
     	})
     })
 	}
 
   render() {
+
   	const { user } = this.state;
+
     return (
       <div className="App">
       <div>
-    		<p>Github Search station</p>
+    		<h1>Github Search station</h1>
     	</div>
       	<p>Enter a username to get a profile info and repositories </p>
     		<div className="search">
     			<input type="text"
     						 placeholder="Enter a GitHub Username" ref="name"/>
     			<button onClick={this.getUser}>Go</button>
-    		</div>
-    		<div className="result">
-    			<Profile user={user}/>
+          {this.state.showComponent ?
+            <Profile user={user}/>:
+            null
+          }
     		</div>
       </div>
     );
   }
 }
+
+
 
 export default App;
--- src/Profile.js
+++ src/Profile.js
@@ -1,15 +1,27 @@
 import React, { Component } from 'react';
-
+import App from './App';
 class Profile extends Component {
 
-
+ 
 	render() {
-		const { user } = this.props
+
+		const { user } = this.props;
 		return (
-			<div>
-				<h3>User Details</h3>
-				<h3>{user.name}</h3>
-				<h3>{user.location}</h3>
+			
+			<div className="profile">	
+				<h3 className ="title">User Details</h3>
+		    <div className="image">
+		    	<img src={user.image}/>
+		    	<br/>
+		    	<a target="_blank" href={user.link}>go to profile</a>
+		    </div>
+				<div className="info">
+					<p><strong>Name:</strong> {user.name}</p>
+					<p><strong>Location:</strong> {user.location}</p>
+					<p><strong>Public repos:</strong> {user.repos}</p>
+					<p><strong>Followers:</strong> {user.followers}</p>
+					<p><strong>Member since:</strong> {user.since}</p>
+				</div>
 			</div>
 		)
 	}
